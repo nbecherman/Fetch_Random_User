06c7a4d7f657490772de94fced5a383d0ec46127
NICOLÁS DAVID BECHERMAN <47700068@ort>
2024-05-29 12:06:29 -0300

    Primer commit, lista con nombres

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+
+function UsersList() {
+  const urlApi = "https://randomuser.me/api/?results=5";
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(urlApi)
+      .then(response => response.json())
+      .then(data => setUsers(data.results))
+      .catch(error => console.log('Hubo un error ' + error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Listado:</h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user.name.first}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <UsersList />
       </header>
     </div>
   );
 }
 
 export default App;
+
