5ba46d2b4c7883e75ee9b2e3dd855ae9477aa89a
NICOLÁS DAVID BECHERMAN <47700068@ort>
2024-06-05 11:28:39 -0300

    grand finale

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+function Modal({ user, onClose }) {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close-btn" onClick={onClose}>&times;</span>
+        <img src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} />
+        <h2>{user.name.first} {user.name.last}</h2>
+        <p>Email: {user.email}</p>
+        <p>Phone: {user.phone}</p>
+        <p>Location: {user.location.city}, {user.location.country}</p>
+      </div>
+    </div>
+  );
+}
+
 function UsersList() {
   const urlApi = "https://randomuser.me/api/?results=5";
   const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
 
   useEffect(() => {
     fetch(urlApi)
@@ -12,6 +28,14 @@ function UsersList() {
       .then(data => setUsers(data.results))
       .catch(error => console.log('Hubo un error ' + error));
   }, []);
+
+  const handleOpenModal = (user) => {
+    setSelectedUser(user);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedUser(null);
+  };
 
   return (
     <div>
@@ -22,20 +46,20 @@ function UsersList() {
             <div className="card-content">
               <img src={user.picture.medium} alt={`${user.name.first} ${user.name.last}`} />
               <p>{user.name.first} {user.name.last}</p>
+              <button className="open-modal-btn" onClick={() => handleOpenModal(user)}>Mas info</button>
             </div>
           </div>
         ))}
       </div>
+      {selectedUser && <Modal user={selectedUser} onClose={handleCloseModal} />}
     </div>
   );
 }
 
 function App() {
   return (
-        <UsersList />
+    <UsersList />
   );
 }
 
 export default App;
-
-
